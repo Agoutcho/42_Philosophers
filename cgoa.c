280c0f44b398a2a38a5a0c16753cfa427bf6dd3a
Agoutcho <91107537+Agoutcho@users.noreply.github.com>
2022-12-26 12:41:46 +0100

    Update cgoa.c

--- cgoa.c
+++ cgoa.c
@@ -2,76 +2,133 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
 
-#define MAX_PHILOSOPHERS 100
-#define DEFAULT_EAT_COUNT -1
-#define DEFAULT_SLEEP_TIME 1000
-#define DEFAULT_EAT_TIME 1000
-#define DEFAULT_DIE_TIME 10000
+#define UNUSED(x) (void)(x)
 
-// Data structure to represent a philosopher
+typedef enum {
+    THINKING,
+    EATING,
+    SLEEPING,
+    DEAD
+} philosopher_state_t;
+
 typedef struct {
-  int id;
-  int eat_count;
-  int sleep_time;
-  int eat_time;
-  int die_time;
-  int left_fork;
-  int right_fork;
+    pthread_t thread;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
+    int id;
+    int num_times_eaten;
+    philosopher_state_t state;
+    unsigned int time_to_die;
+    unsigned int time_to_eat;
+    unsigned int time_to_sleep;
+    unsigned int num_times_to_eat;
 } philosopher_t;
 
-// Array of philosophers
-philosopher_t philosophers[MAX_PHILOSOPHERS];
+void *philosopher_thread_func(void *arg)
+{
+    philosopher_t *philosopher = (philosopher_t*)arg;
+    unsigned int time_since_last_meal = 0;
+    int ret;
+    struct timespec ts;
 
-// Number of philosophers
-int num_philosophers;
+    while (1) {
+        philosopher->state = THINKING;
+        printf("%u Philosopher %d is thinking\n", (unsigned int)time(NULL), philosopher->id);
 
-// Mutex to protect access to forks
-pthread_mutex_t forks[MAX_PHILOSOPHERS];
+        ret = pthread_mutex_trylock(philosopher->left_fork);
+        if (ret != 0) {
+            continue;
+        }
 
-// Mutex to protect access to the log
-pthread_mutex_t log_mutex;
+        ret = pthread_mutex_trylock(philosopher->right_fork);
+        if (ret != 0) {
+            pthread_mutex_unlock(philosopher->left_fork);
+            continue;
+        }
 
-// Function that represents the behavior of a philosopher
-void * philosopher_thread(void *arg) {
-  philosopher_t *p = (philosopher_t*)arg;
+        philosopher->state = EATING;
+        printf("%u Philosopher %d is eating\n", (unsigned int)time(NULL), philosopher->id);
 
-  while (1) {
-    // Wait some time before thinking
-    usleep(p->sleep_time * 1000);
+        ts.tv_sec = philosopher->time_to_eat / 1000;
+        ts.tv_nsec = (philosopher->time_to_eat % 1000) * 1000000;
+        nanosleep(&ts, NULL);
 
-    // Try to get the left fork
-    pthread_mutex_lock(&forks[p->left_fork]);
-    printf("%ld %d has taken left fork\n", time(NULL), p->id);
+        pthread_mutex_unlock(philosopher->left_fork);
+        pthread_mutex_unlock(philosopher->right_fork);
 
-    // Try to get the right fork
-    pthread_mutex_lock(&forks[p->right_fork]);
-    printf("%ld %d has taken right fork\n", time(NULL), p->id);
+        philosopher->num_times_eaten++;
+        time_since_last_meal = 0;
 
-    // Eat for some time
-    printf("%ld %d is eating\n", time(NULL), p->id);
-    usleep(p->eat_time * 1000);
-    p->eat_count++;
+        if (philosopher->num_times_to_eat > 0 && philosopher->num_times_eaten >= philosopher->num_times_to_eat) {
+            break;
+        }
 
-    // Release the forks
-    pthread_mutex_unlock(&forks[p->left_fork]);
-    pthread_mutex_unlock(&forks[p->right_fork]);
-  }
+        philosopher->state = SLEEPING;
+        printf("%u Philosopher %d is sleeping\n", (unsigned int)time(NULL), philosopher->id);
+
+        ts.tv_sec = philosopher->time_to_sleep / 1000;
+        ts.tv_nsec = (philosopher->time_to_sleep % 1000) * 1000000;
+        nanosleep(&ts, NULL);
+
+        time_since_last_meal += philosopher->time_to_sleep;
+        if (time_since_last_meal >= philosopher->time_to_die) {
+            philosopher->state = DEAD;
+            printf("%u Philosopher %d died\n", (unsigned int)time(NULL), philosopher->id);
+            break;
+        }
+    }
+
+    return NULL;
 }
 
-int main(int argc, char *argv[]) {
-  // Parse command line arguments
-  if (argc < 4) {
-    printf("Usage: %s number_of_philosophers time_to_die time_to_eat [time_to_sleep] [number_of_times_each_philosopher_must_eat]\n", argv[0]);
-    return 1;
-  }
-  num_philosophers = atoi(argv[1]);
-  int die_time = atoi(argv[2]);
-  int eat_time = atoi(argv[3]);
-  int sleep_time = DEFAULT_SLEEP_TIME;
-  int eat_count = DEFAULT_EAT_COUNT;
-  if (argc > 4) {
-    sleep_time = atoi(argv[4]);
-  }
-  if (argc > 5) {
-    eat_count = atoi(argv[5
+int main(int argc, char *argv[])
+{
+    int num_philosophers;
+    unsigned int time_to_die;
+    unsigned int time_to_eat;
+    unsigned int time_to_sleep;
+    unsigned int num_times_to_eat = 0;
+    pthread_mutex_t forks[100];
+    philosopher_t philosophers[100];
+
+    if (argc < 5 || argc > 6) {
+        fprintf(stderr, "Usage: %s num_philosophers time_to_die time_to_eat time_to_sleep [num_times_to_eat]\n", argv[0]);
+        return 1;
+    }
+
+    num_philosophers = atoi(argv[1]);
+    time_to_die = atoi(argv[2]);
+    time_to_eat = atoi(argv[3]);
+    time_to_sleep = atoi(argv[4]);
+    if (argc == 6) {
+        num_times_to_eat = atoi(argv[5]);
+    }
+
+    for (int i = 0; i < num_philosophers; i++) {
+        pthread_mutex_init(&forks[i], NULL);
+    }
+
+    for (int i = 0; i < num_philosophers; i++) {
+        philosophers[i].left_fork = &forks[i];
+        philosophers[i].right_fork = &forks[(i + 1) % num_philosophers];
+        philosophers[i].id = i + 1;
+        philosophers[i].num_times_eaten = 0;
+        philosophers[i].time_to_die = time_to_die;
+        philosophers[i].time_to_eat = time_to_eat;
+        philosophers[i].time_to_sleep = time_to_sleep;
+        philosophers[i].num_times_to_eat = num_times_to_eat;
+        pthread_create(&philosophers[i].thread, NULL, philosopher_thread_func, &philosophers[i]);
+    }
+
+    for (int i = 0; i < num_philosophers; i++) {
+        pthread_join(philosophers[i].thread, NULL);
+    }
+
+    for (int i = 0; i < num_philosophers; i++) {
+        pthread_mutex_destroy(&forks[i]);
+    }
+
+    return 0;
+}
