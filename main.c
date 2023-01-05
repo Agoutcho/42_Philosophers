679988ff6514011ad7fb2743b40ef0dd2609a845
Atchougo <atchougo@student.42lyon.fr>
2023-01-05 19:07:46 +0100

    Bon calcul du temp

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 18:10:31 by atchougo          #+#    #+#             */
-/*   Updated: 2023/01/04 22:45:41 by atchougo         ###   ########.fr       */
+/*   Updated: 2023/01/05 19:06:33 by atchougo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ struct s_dataa{
         int time;
 };
 
-void accurate_usleep(int usec)
+void accurate_msleep(unsigned long long usec)
 {
     struct timeval tp;
     int time;
+    
     gettimeofday(&tp, NULL);
     time = tp.tv_usec;
-    while (tp.tv_usec - time < usec)
+    while (tp.tv_usec - time < (int)(usec * 1000))
     {
         gettimeofday(&tp, NULL);
         usleep(1);
@@ -42,7 +43,7 @@ void *fct(void *arg)
     // {
     //     gettimeofday(&tp, NULL);
     //     printf("[%s] Before usleep time : %d\n",__FUNCTION__,  tp.tv_usec - deado->time);
-    //     accurate_usleep(15);
+    //     accurate_msleep(15);
     //     i++;
     // }
     t_data *data;
@@ -54,6 +55,43 @@ void *fct(void *arg)
     return NULL;
 }
 
+void destroy(t_data *data)
+{
+    pthread_mutex_destroy(&data->mutex);
+}
+
+void *ft_philo(void *phil)
+{
+    t_philo *data;
+    int temp;
+
+    data = (t_philo *)phil;
+    temp = (data->id + 1) % data->data->nbr_of_philo;
+    if (!pthread_mutex_lock(&data->mutex) && !pthread_mutex_lock(&data->data->philo[temp].mutex))
+    {
+        printf("%d Philosopher %d has taken a fork", time_in_ms(1, data->data), data->id + 1);
+        accurate_msleep(data->data->time_to_eat);
+        pthread_mutex_unlock(&data->mutex);
+        pthread_mutex_unlock(&data->data->philo[temp].mutex);
+    }
+    return (NULL);
+}
+
+int time_in_ms(int value, t_data *data)
+{
+    struct timeval tp;
+    
+    gettimeofday(&tp, NULL);
+    if (value)
+    {
+        printf("[%s] data.time : %d\n",__FUNCTION__, data->time);
+        printf("[%s] tp.tv_sec : %d\n",__FUNCTION__, tp.tv_usec);
+        return ((tp.tv_usec - data->time) / 1000);
+    }
+    else 
+        return (tp.tv_usec);
+}
+
 int init(int argc, char **argv, t_data *data)
 {
     if (!init_value(argc, argv, data))
@@ -61,44 +99,40 @@ int init(int argc, char **argv, t_data *data)
     return (1);
 }
 
-void destroy(t_data *data)
+int main(int argc, char **argv)
 {
-    pthread_mutex_destroy(&data->mutex);
-}
-
-int main()
-{
+    (void)argc;
+    (void)argv;
     t_data data;
-    pthread_t thread1;
-    
-    // if (!init(argc, argv, &data))
-    //     return (0);
-    pthread_mutex_init(&data.mutex, NULL);
-    pthread_mutex_lock(&data.mutex);
+
+    // init(argc, argv, &data);
+    data.time = time_in_ms(0, &data);
     printf("[%s] START\n",__FUNCTION__);
+    printf("[%s] data.time : %d\n",__FUNCTION__, data.time);
+    printf("[%s] actual time : %d\n",__FUNCTION__, time_in_ms(1, &data));
+    printf("[%s] GO sleep\n",__FUNCTION__);
+    accurate_msleep(100);
+    printf("[%s] 1 STOP sleep\n",__FUNCTION__);
+    printf("[%s] actual time : %d\n",__FUNCTION__, time_in_ms(1, &data));
+    accurate_msleep(256);
+    printf("[%s] 2 STOP sleep\n",__FUNCTION__);
+    printf("[%s] actual time : %d\n",__FUNCTION__, time_in_ms(1, &data));
+    accurate_msleep(322);
+    printf("[%s] 3 STOP sleep\n",__FUNCTION__);
+    printf("[%s] actual time : %d\n",__FUNCTION__, time_in_ms(1, &data));
+    return 0;
+    // // if (!init(argc, argv, &data))
+    // //     return (0);
+    // pthread_mutex_init(&data.mutex, NULL);
+    // pthread_mutex_lock(&data.mutex);
+    // printf("[%s] START\n",__FUNCTION__);
     
-    pthread_create(&thread1, NULL, fct, &data);
-    accurate_usleep(700);
-    printf("[%s] Created\n",__FUNCTION__);
-    pthread_mutex_unlock(&data.mutex);
-    pthread_join(thread1, NULL);
-    destroy(&data.mutex);
-    // struct s_dataa data;
-    // pthread_t thread1;
-    // struct timeval tp;
-    // data.is_dead = 0;
-    // printf("\n[%s] GO Thread\n",__FUNCTION__);
-    // gettimeofday(&tp, NULL);
-    // data.time = tp.tv_usec;
-    // gettimeofday(&tp, NULL);
-    // printf("[%s] Before Create time : %d\n",__FUNCTION__,  tp.tv_usec - data.time);
-    // gettimeofday(&tp, NULL);
-    // printf("[%s] After Create time : %d\n",__FUNCTION__,  tp.tv_usec - data.time);
-    // accurate_usleep(150);
-    // data.is_dead = 1;
-    // gettimeofday(&tp, NULL);
-    // printf("[%s] Finish time : %d\n",__FUNCTION__,  tp.tv_usec - data.time);
+    // pthread_create(&thread1, NULL, fct, &data);
+    // accurate_msleep(700);
+    // printf("[%s] Created\n",__FUNCTION__);
+    // pthread_mutex_unlock(&data.mutex);
     // pthread_join(thread1, NULL);
+    // destroy(&data.mutex);
     
     return (0);
 }
--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: atchougo <atchougo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 18:08:01 by atchougo          #+#    #+#             */
-/*   Updated: 2023/01/04 22:31:53 by atchougo         ###   ########.fr       */
+/*   Updated: 2023/01/05 19:01:06 by atchougo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # include <pthread.h>
 # include <sys/time.h>
 
+typedef struct s_data t_data;
+
 typedef enum e_state{
     e_EATING,
     e_SLEEPING,
@@ -30,6 +32,9 @@ typedef enum e_state{
 typedef struct s_philo {
     int id;
     t_state state;
+    pthread_mutex_t mutex;
+    pthread_t thread;
+    t_data *data;
 } t_philo;
 
 typedef struct s_data {
@@ -38,10 +43,13 @@ typedef struct s_data {
     int time_to_eat;
     int time_to_sleep;
     int nbr_must_eat;
+    int time;
     pthread_mutex_t mutex;
+    t_philo *philo;
 } t_data;
 
 int is_parsing_ok(int argc, char **argv);
 int init_value(int argc, char **argv, t_data *data);
+int time_in_ms(int value, t_data *data);
 
 #endif
